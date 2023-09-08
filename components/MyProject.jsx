74e15b0dae9a6b2b716abd894b04cecb4ac1e1bf
hSangg <sangfc774@gmail.com>
2023-09-08 23:48:26 +0700

    update

--- components/MyProject.jsx
+++ components/MyProject.jsx
@@ -1,47 +1,52 @@
-import { motion, useMotionValue } from "framer-motion";
-import Folder from "./Folder";
+import {
+  motion,
+  useMotionValue,
+} from 'framer-motion'
+import Folder from './Folder'
 const dataFolder = [
   {
-    name: "Tình yêu có nghĩa là gì",
-    link: "",
+    name: 'Tình yêu có nghĩa là gì',
+    link: '',
   },
   {
-    name: "resource",
-    link: "/resource",
+    name: 'resource',
+    link: '/resource',
   },
   {
-    name: "????????",
-    link: "???????",
+    name: 'gallery',
+    link: '/artistic/design/gallery',
   },
 
   {
-    name: "????????",
-    link: "???????",
+    name: '????????',
+    link: '???????',
   },
-];
+]
 
 const MyProject = () => {
-  let mouseX = useMotionValue(0);
+  let mouseX = useMotionValue(0)
 
   return (
-    <div className="relative mb-20">
+    <div className='relative mb-20'>
       <motion.div
         onMouseMove={(e) => {
-          mouseX.set(e.pageX);
+          mouseX.set(e.pageX)
         }}
-        className="flex gap-2 sm:gap-10 absolute left-[50%] translate-x-[-50%]"
+        className='flex gap-2 sm:gap-10 absolute left-[50%] translate-x-[-50%]'
       >
-        {dataFolder.map(({ name, link }, index) => (
-          <Folder
-            mouseX={mouseX}
-            name={name}
-            src={link}
-            key={index}
-          />
-        ))}
+        {dataFolder.map(
+          ({ name, link }, index) => (
+            <Folder
+              mouseX={mouseX}
+              name={name}
+              src={link}
+              key={index}
+            />
+          )
+        )}
       </motion.div>
     </div>
-  );
-};
+  )
+}
 
-export default MyProject;
+export default MyProject
--- components/SangText.jsx
+++ components/SangText.jsx
@@ -1,26 +1,30 @@
-import { motion } from "framer-motion";
-import Image from "next/image";
+import { motion } from 'framer-motion'
+import Image from 'next/image'
 const SangText = () => {
   return (
     <motion.div
-      initial={{ y: "-50px", opacity: 0 }}
+      initial={{ y: '-50px', opacity: 0 }}
       whileInView={{
         y: 0,
         opacity: 1,
-        transition: { type: "spring", bounce: 0.5, duration: 1 },
+        transition: {
+          type: 'spring',
+          bounce: 0.5,
+          duration: 1,
+        },
       }}
-      className="flex items-center justify-center "
+      className='flex items-center justify-center '
     >
       <Image
-        src="/sang.jpg"
-        width="1300px"
-        height="500px"
-        objectFit="contain"
-        loading="lazy"
-        alt=""
+        src='/my-name.jpg'
+        width='1300px'
+        height='500px'
+        objectFit='contain'
+        loading='lazy'
+        alt=''
       />
     </motion.div>
-  );
-};
+  )
+}
 
-export default SangText;
+export default SangText
